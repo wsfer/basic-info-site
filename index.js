2d97dbb132a31092fa2e158965ea74236563c715
wsfer <93054415+wsfer@users.noreply.github.com>
2024-07-18 16:49:24 -0300

    feat: create logic to send html files on request

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const http = require('node:http');
+const fs = require('node:fs/promises');
 
 const server = http.createServer();
 const PORT = process.env.PORT || 3000;
 
-server.on('request', (req, res) => {
-    res.end("It's working!");
+server.on('request', async (req, res) => {
+  let filePath = null;
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+
+  switch (req.url) {
+    case '/':
+      filePath = 'index.html';
+      break;
+    case '/about':
+      filePath = 'about.html';
+      break;
+    case '/contact-me':
+      filePath = 'contact-me.html';
+      break;
+    default:
+      filePath = '404.html';
+      res.statusCode = 404;
+  }
+
+  try {
+    const file = await fs.readFile(filePath);
+    res.setHeader('Content-Legth', Buffer.byteLength(file));
+    res.write(file);
+  } catch (error) {
+    console.error(error);
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('500 Internal Server Error');
+  }
+
+  res.end();
 });
 
 server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });

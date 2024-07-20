99918792be096759f646e036bb67b3850bb44609
wsfer <93054415+wsfer@users.noreply.github.com>
2024-07-19 23:28:19 -0300

    fix: http content type for favicon.ico

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const routes = new Map([
   ['/', { fileName: 'index.html', contentType: 'text/html' }],
   ['/about', { fileName: 'about.html', contentType: 'text/html' }],
   ['/contact-me', { fileName: 'contact-me.html', contentType: 'text/html' }],
-  ['/favicon.ico', { fileName: 'favicon.ico', contentType: 'images/x-icon' }],
+  ['/favicon.ico', { fileName: 'favicon.ico', contentType: 'image/x-icon' }],
   ['/style.css', { fileName: 'style.css', contentType: 'text/css' }],
 ]);
 

bd43c3203fd16745e25e8b4e76cba7c12581ebe7
wsfer <93054415+wsfer@users.noreply.github.com>
2024-07-19 23:04:18 -0300

    feat: create routes module to map url pathname to files and content type

---
+++ routes.js
@@ -0,0 +1,9 @@
+const routes = new Map([
+  ['/', { fileName: 'index.html', contentType: 'text/html' }],
+  ['/about', { fileName: 'about.html', contentType: 'text/html' }],
+  ['/contact-me', { fileName: 'contact-me.html', contentType: 'text/html' }],
+  ['/favicon.ico', { fileName: 'favicon.ico', contentType: 'images/x-icon' }],
+  ['/style.css', { fileName: 'style.css', contentType: 'text/css' }],
+]);
+
+module.exports = routes;

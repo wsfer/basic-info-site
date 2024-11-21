07a678309916ab7dc6fc5d50cf79bc49850797c3
wsfer <93054415+wsfer@users.noreply.github.com>
2024-11-21 15:14:04 -0300

    docs: add some comments on main file

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 3000;
 // For images, javascript and CSS files
 app.use(express.static(path.join(process.cwd(), 'public')));
 
+/**
+ * Also HTML files could be static served by changing the links on frontend to include the '.html' at the end.
+ * Each page has it's own route just for learning purposes.
+ */
 app.get('/', (req, res) => res.sendFile(path.join(process.cwd(), 'pages/index.html')));
 app.get('/about', (req, res) => res.sendFile(path.join(process.cwd(), 'pages/about.html')));
 app.get('/contact-me', (req, res) => res.sendFile(path.join(process.cwd(), 'pages/contact-me.html')));
